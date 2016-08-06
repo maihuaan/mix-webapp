2f71789ba32b1c867fc6b50241ff2009bee71e7e
maihuaan <maihuaan@126.com>
2016-08-06 14:16:10 +0800

    首次提交mix-webapp

---
+++ src/main/java/com/huaan/mixwebapp/rest/HelloWorldService.java
@@ -0,0 +1,22 @@
+package com.huaan.mixwebapp.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+
+@Path("/hello")
+public class HelloWorldService 
+{
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Restful  say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
